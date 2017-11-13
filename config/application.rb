9f5c326ce8eda8e937c4b2c7e65bcaa47150858f
Nathan Anderson <nathan.anderson87@gmail.com>
2017-11-13 20:33:43 +0900

    CK E Fix

--- config/application.rb
+++ config/application.rb
@@ -24,7 +24,9 @@ module GTreks
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
-
+    config.assets.precompile += CKeditor.assets
+    config.assets.precompile += %w( ckeditor/* )
+    config.autoload_paths += %W(#{config.root}/app/models/ckeditor)
     # Don't generate system test files.
     config.generators.system_tests = nil
   end
